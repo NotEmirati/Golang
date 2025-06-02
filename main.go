60cd24a0849041aadf30074af27551ec231a6b21
Denis Polozov <denis.polozov@b2broker.com>
2025-06-02 07:23:01 +0400

    just testing new stuff with variables

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Goodbye, World")
+	var x int
+	var y string = `new line
+test`
+	z := 13.54
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
 }
